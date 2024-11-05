8b542826ce7f1ca9ef302155b71e9b72ddc84eb4
Maher Fayad <63962002+MaherFayad@users.noreply.github.com>
2024-11-05 19:56:55 +0300

    404 Fix

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,39 @@
 // /pages/404.js
-import Rive from '@rive-app/react-canvas';
 import Layout from '../../Components/Layout/Layout';
+import ButtonComponent from "../../Components/Sections/Home/ButtonComponent";
+import { useRouter } from 'next/router';
 
-const NotFound = () => (
-  <Layout title="404" description="Page Not found">
-    <Rive
-      src="/404.riv"
-      stateMachines="State Machine 1"
-      style={{ width: '100vw', height: '100vh' }}
-    />
-  </Layout>
-);
+const NotFound = () => {
+  const router = useRouter();
+
+  const handleNavigateHome = () => {
+    router.push('/');
+  };
+
+  return (
+    <Layout title="404" description="Page Not found">
+      <div className="Container404">
+        <div className="spacer404"></div>
+        <div className="bar">
+          <div className="ball"></div>
+        </div>
+        <h1 className="text-gradient uxup-fs-h2 fw-bold">
+          <br /> Go back to where the magic happens.
+        </h1>
+        <h3 className="fw-normal uxup-color-text-50 uxup-fs-paragraph my-4">
+          We couldn't find the page you were looking for. But don't worry, our team is great at finding creative solutions—and we'll help you get back on track.
+        </h3>
+        <ButtonComponent
+          id="learn-more"
+          type="primary"
+          text="Back to home"
+          onClick={handleNavigateHome}
+          isDisable={false}
+          showLoading={false}
+        />
+      </div>
+    </Layout>
+  );
+};
 
 export default NotFound;
--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -4,11 +4,10 @@ import Layout from '../../../Components/Layout/Layout';
 
 const NotFound = () => (
   <Layout title="404" description="Page Not found">
-    <Rive
-      src="/404.riv"
-      stateMachines="State Machine 1"
-      style={{ width: '100vw', height: '100vh' }}
-    />
+    <div className="Container404">
+        <div className="bar">
+            <div className="ball"></div>
+        </div></div>
   </Layout>
 );
 
